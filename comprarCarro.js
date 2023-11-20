1b5062f40c2245b4e1dd79d468ad6c6d5de91651
MateusGomesc <129867943+MateusGomesc@users.noreply.github.com>
2023-11-20 17:11:55 -0300

    Add files via upload

--- comprarCarro.js
+++ comprarCarro.js
@@ -1,3 +1,26 @@
-//recebe informações da página de catalogo
-var formData = new FormData(document.querySelector('form'))
-console.log(formData)
+/*recebe informações da página de catalogo*/
+
+//recebe url
+let fullurl = window.location.href
+let param = ''
+
+//recebe parametros
+for(let i=0; i<fullurl.length; i++){
+    if(fullurl[i] === '?'){
+        param = fullurl.substring(i+1).replace('+', ' ')
+    }
+}
+
+//recebe marca e modelo do carro
+let car = param.substring(4)
+let marca = ''
+let modelo = ''
+
+//verifica onde está espaço na string
+for(let i=0; i<car.length; i++){
+    if(car[i] === ' '){
+        //divide informações
+        marca = car.slice(0, i)
+        modelo = car.slice(i+1)
+    }
+}
